ee92bdcbbce7dc13888436e2e5a0930333ce5a60
iainquayle <36175014+iainquayle@users.noreply.github.com>
2020-02-15 19:09:02 -0700

    Add files via upload
    added some comments

--- Layer.java
+++ Layer.java
@@ -5,19 +5,19 @@ public class Layer
 	protected static float[] valsAch;
 	protected static float[] valsReq;
 	protected int lenVals;
-	protected int begVals;
-	protected int endVals;
+	protected int begVals; //beginning index of vals
+	protected int endVals; //ending index of vals
 	
 	protected float[] weights;
 	protected int lenWeis;
 	
-	protected static float[] visValsAch;
-	protected static float[] visValsReq;
+	protected static float[] visValsAch; //this is now the same pointer as valsAch, but this has been kept for code readability, it may be done away with
+	protected static float[] visValsReq; //this is now the same pointer as valsReq, but this has been kept for code readability, it may be done away with
 	protected int lenValsVis;
-	protected int begValsVis;
-	protected int endValsVis;
+	protected int begValsVis; //beginning index of valsVis
+	protected int endValsVis; //ending index of valsVis
 	
-	protected static float[] threadSplits;
+	protected static float[] threadSplits; //the pre-calculated splits of where each thread ends and begins, may be changed to be calculated on the fly for cache hits
 	
 	protected float learnRate;
 	
