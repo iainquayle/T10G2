b0ab07065dede6b2f319e324575b49c96f70d0e7
ParthPatel100 <60551469+ParthPatel100@users.noreply.github.com>
2020-02-21 12:36:18 -0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -18,11 +18,10 @@ public class CalculatorV2 {
 		return formulaSplitAll;
 	}
 
-
 	
 	public static void division(ArrayList<String> m) {
 		for (int i = 0 ; i <= m.size() - 1; i ++) {
-			if (m.get(i).equals("/")) {
+			while (m.get(i).equals("/")) {
 				int indexOfThatSymbol = m.indexOf("/");
 				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
 				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
@@ -34,13 +33,14 @@ public class CalculatorV2 {
 				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
 				m.add(indexOfThatSymbol - 1, changed);
 				System.out.println(m);
+				i = 0;
 				}	
 		}
 	}
 	
 	public static void multiplication(ArrayList<String> m) {
 		for (int i = 0 ; i <= m.size() - 1; i ++) {
-			if (m.get(i).equals("*")) {
+			while (m.get(i).equals("*")) {
 				int indexOfThatSymbol = m.indexOf("*");
 				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
 				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
@@ -52,13 +52,14 @@ public class CalculatorV2 {
 				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
 				m.add(indexOfThatSymbol - 1, changed);
 				System.out.println(m);
+				i = 0;
 				}	
 		}
 	}
 	
 	public static void addition(ArrayList<String> m) {
 		for (int i = 0 ; i <= m.size() - 1; i ++) {
-			if (m.get(i).equals("+")) {
+			while(m.get(i).equals("+")) {
 				int indexOfThatSymbol = m.indexOf("+");
 				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
 				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
@@ -70,12 +71,13 @@ public class CalculatorV2 {
 				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
 				m.add(indexOfThatSymbol - 1, changed);
 				System.out.println(m);
+				i = 0;
 				}	
 		}
 	}
 	public static void subtraction(ArrayList<String> m) {
 		for (int i = 0 ; i <= m.size() - 1; i ++) {
-			if (m.get(i).equals("-")) {
+			while(m.get(i).equals("-")) {
 				int indexOfThatSymbol = m.indexOf("-");
 				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
 				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
@@ -87,19 +89,19 @@ public class CalculatorV2 {
 				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
 				m.add(indexOfThatSymbol - 1, changed);
 				System.out.println(m);
+				i = 0;
 				}	
 		}
 	}
 	
 	public static void main(String[] args) {
-		String testCase = "4 + 2 + 2 + 2 + 2";
+		String testCase = "4 + 2 - 2 * 10 + 4 - 10 + 100 - 9 * 5";
 		ArrayList<String> formulaSplit = converStrToList(testCase);
-		while (formulaSplit.size() != 1) {
-			division(formulaSplit);
-			multiplication(formulaSplit);
-			addition(formulaSplit);
-			subtraction(formulaSplit);
-		}
+		System.out.println(formulaSplit);
+		division(formulaSplit);
+		multiplication(formulaSplit);
+		addition(formulaSplit);
+		subtraction(formulaSplit);
 		System.out.println(formulaSplit);
 		
 		
