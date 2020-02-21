6316c1109bfaa10df798464c4f68a63b640f82d7
ParthPatel100 <60551469+ParthPatel100@users.noreply.github.com>
2020-02-21 11:24:12 -0700

    Cleaned up the code
    left with brackets operator and giving the output in a more improved manner

--- Calculator.java
+++ Calculator.java
@@ -1,91 +1,108 @@
-
+package engine;
 
 import java.util.ArrayList;
 
-public class Calculator{
-	public static void main(String[] args) {
-		String testCase1 = "2 + 2 + 2 + 3";
-		String[] ArrayForm = testCase1.split(" ");
-		ArrayList<String> formulaSplit = new ArrayList<String>();
+public class CalculatorV2 {
+	static ArrayList<String> formulaSplitAll = new ArrayList<String>();
+	static String[] ArrayForm;
+	static ArrayList<String> formulaSplitInBrackets = new ArrayList<String>();
+	static ArrayList<String> formulaSplitOutBrackets = new ArrayList<String>();
+	static String caldNum = null;
+	
+	public static ArrayList<String> converStrToList(String testCase){
+		ArrayForm = testCase.split(" ");
+//		ArrayList<String> formulaSplitInBrackets = new ArrayList<String>();
 		for (int i = 0; i < ArrayForm.length; i++ ) {
-			formulaSplit.add(ArrayForm[i]);
-//			System.out.println(formulaSplit);
+			formulaSplitAll.add(ArrayForm[i]);
 		}
-		System.out.println(formulaSplit);
+		return formulaSplitAll;
+	}
 
-		for (int i = 0 ; i <= ArrayForm.length - 1; i ++) {
-			if (ArrayForm[i].equals("/")) {
-				int indexOfThatSymbol = formulaSplit.indexOf("/");
-				double num1 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol - 1));
-				double num2 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol + 1));
+
+	
+	public static void division(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			if (m.get(i).equals("/")) {
+				int indexOfThatSymbol = m.indexOf("/");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
 				double num3 = (num1 / num2);
 				String changed = Double.toString(num3);
 //				System.out.println(num3);
 //				System.out.println(num1);
 //				System.out.println(num2);
-				formulaSplit.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
-				formulaSplit.add(indexOfThatSymbol - 1, changed);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				System.out.println(m);
 				}	
 		}
-		System.out.println(formulaSplit);
-		for (int i = 0 ; i <= ArrayForm.length - 1; i ++) {
-			if (ArrayForm[i].equals("*")) {
-				int indexOfThatSymbol = formulaSplit.indexOf("*");
-				double num1 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol - 1));
-				double num2 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol + 1));
+	}
+	
+	public static void multiplication(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			if (m.get(i).equals("*")) {
+				int indexOfThatSymbol = m.indexOf("*");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
 				double num3 = (num1 * num2);
 				String changed = Double.toString(num3);
 //				System.out.println(num3);
 //				System.out.println(num1);
 //				System.out.println(num2);
-				formulaSplit.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
-				formulaSplit.add(indexOfThatSymbol - 1, changed);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				System.out.println(m);
 				}	
 		}
-		System.out.println(formulaSplit);
-		for (int i = 0 ; i <= ArrayForm.length - 1; i ++) {
-			if (ArrayForm[i].equals("+")) {
-				int indexOfThatSymbol = formulaSplit.indexOf("+");
-				double num4 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol - 1));
-				double num5 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol + 1));
-				double num6 = (num4 + num5);
-				String changed = Double.toString(num6);
-//				System.out.println(num6);
-//				System.out.println(num4);
-//				System.out.println(num5);
-				formulaSplit.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
-				formulaSplit.add(indexOfThatSymbol - 1, changed);
-				}		
-		}
-		System.out.println(formulaSplit);
-			
-		for (int i = 0 ; i <= ArrayForm.length - 1; i ++) {
-			if (ArrayForm[i].equals("-")) {
-				int indexOfThatSymbol = formulaSplit.indexOf("-");
-				double num4 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol - 1));
-				double num5 = Double.parseDouble(formulaSplit.get(indexOfThatSymbol + 1));
-				double num6 = (num4 -num5);
-				String changed = Double.toString(num6);
-//				System.out.println(num6);
-//				System.out.println(num4);
-//				System.out.println(num5);
-				formulaSplit.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
-				formulaSplit.add(indexOfThatSymbol - 1, changed);
+	}
+	
+	public static void addition(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			if (m.get(i).equals("+")) {
+				int indexOfThatSymbol = m.indexOf("+");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = (num1 + num2);
+				String changed = Double.toString(num3);
+//				System.out.println(num3);
+//				System.out.println(num1);
+//				System.out.println(num2);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				System.out.println(m);
 				}	
 		}
-				
+	}
+	public static void subtraction(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			if (m.get(i).equals("-")) {
+				int indexOfThatSymbol = m.indexOf("-");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = (num1 - num2);
+				String changed = Double.toString(num3);
+//				System.out.println(num3);
+//				System.out.println(num1);
+//				System.out.println(num2);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				System.out.println(m);
+				}	
+		}
+	}
+	
+	public static void main(String[] args) {
+		String testCase = "4 + 2 + 2 + 2 + 2";
+		ArrayList<String> formulaSplit = converStrToList(testCase);
+		while (formulaSplit.size() != 1) {
+			division(formulaSplit);
+			multiplication(formulaSplit);
+			addition(formulaSplit);
+			subtraction(formulaSplit);
+		}
 		System.out.println(formulaSplit);
-
+		
 		
 	}
+	
 }
-
-
-
-
-
-
-
-
-
-
