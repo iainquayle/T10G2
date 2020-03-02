f40c4a094b290d8da316154b5ea15aeecdd921c7
iainquayle <36175014+iainquayle@users.noreply.github.com>
2020-03-01 17:20:23 -0700

    Changed Float[] to float[], removed external lib

--- TextNet/InputData.java
+++ TextNet/InputData.java
@@ -1,20 +1,16 @@
-package inputData;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+package engine;
+
+import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
+
 /*
  * This class uses the external openCSV library.
  * http://opencsv.sourceforge.net/
  */
 public class InputData {
-	
-	@SuppressWarnings("deprecation")
 	/**
 	 * Hideous code ahead...
 	 * @param file is a string indicating the file path for FileReader
@@ -22,36 +18,20 @@ public class InputData {
 	 * @throws IOException 
 	 * @throws CsvException
 	 */
-	
-	public Float[] csvToFloatArray(String file) throws IOException, CsvException {
-		 try {
-			FileReader rFile = new FileReader(file);
-			
-			CSVReader readCSV = new CSVReader(rFile);
-			String[] dataString;
-			List<Float> dataFloatList = new ArrayList<Float>();
-			while ((dataString = readCSV.readNext()) != null) { 					//reading the CSV data into a string array
-	            for (String cell : dataString) { 									//adding each element of the string array into a Float ArrayList
-	            	dataFloatList.add(new BigDecimal(cell).setScale(1, BigDecimal.ROUND_CEILING).floatValue());
-
-	                
-	            } 
-	            
-			}
-			Float[] dataFloatArray = new Float[dataFloatList.size()];  
-			dataFloatArray = dataFloatList.toArray(dataFloatArray);  //Converting the Float ArrayList into a Float Array
-			readCSV.close();
-			return dataFloatArray;
-			
-		
-			} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public float[] csvToFloatArray(String file) throws IOException, FileNotFoundException
+	{
+		Scanner in = new Scanner(new File(file));
+		ArrayList<Float> list = new ArrayList<Float>();
+		while(in.hasNextFloat())
+		{
+			list.add(in.nextFloat());
 		}
-		return null;
-		 
-		 
-		 
+		float[] arr = new float[list.size()];
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = list.get(i);
+		}
+		in.close();
+		return arr;
 	}
-
 }
