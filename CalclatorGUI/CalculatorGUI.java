72813f37db4c008388fd0c99e1ab261d4b290898
ParthPatel100 <60551469+ParthPatel100@users.noreply.github.com>
2020-03-06 10:10:21 -0700

    Works like a simple calculator

---
+++ CalclatorGUI/CalculatorGUI.java
@@ -0,0 +1,251 @@
+package engine;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class CalculatorGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField Display;
+	public String testCase;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CalculatorGUI frame = new CalculatorGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @return 
+	 */
+	public CalculatorGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 309, 345);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("1");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Display.setText(Display.getText() + "1");
+			}
+		});
+		btnNewButton.setBounds(15, 75, 50, 42);
+		
+		JButton btnNewButton_1 = new JButton("4");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "4");
+			}
+		});
+		btnNewButton_1.setBounds(15, 120, 50, 42);
+		contentPane.setLayout(null);
+		contentPane.add(btnNewButton);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_1_1 = new JButton("6");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "6");
+			}
+		});
+		btnNewButton_1_1.setBounds(112, 120, 53, 42);
+		contentPane.add(btnNewButton_1_1);
+		
+		JButton btnNewButton_1_2 = new JButton("2");
+		btnNewButton_1_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "2");
+			}
+		});
+		btnNewButton_1_2.setBounds(64, 75, 50, 42);
+		contentPane.add(btnNewButton_1_2);
+		
+		JButton btnNewButton_1_3 = new JButton("3");
+		btnNewButton_1_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "3");
+			}
+		});
+		btnNewButton_1_3.setBounds(112, 75, 53, 42);
+		contentPane.add(btnNewButton_1_3);
+		
+		JButton btnNewButton_1_4 = new JButton("5");
+		btnNewButton_1_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "5");
+			}
+		});
+		btnNewButton_1_4.setBounds(64, 120, 50, 42);
+		contentPane.add(btnNewButton_1_4);
+		
+		JButton btnNewButton_1_5 = new JButton("7");
+		btnNewButton_1_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Display.setText(Display.getText() + "7");
+			}
+			
+		});
+		btnNewButton_1_5.setBounds(15, 165, 50, 42);
+		contentPane.add(btnNewButton_1_5);
+		
+		JButton btnNewButton_1_6 = new JButton("8");
+		btnNewButton_1_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "8");
+			}
+		});
+		btnNewButton_1_6.setBounds(64, 165, 50, 42);
+		contentPane.add(btnNewButton_1_6);
+		
+		JButton btnNewButton_1_7 = new JButton("9");
+		btnNewButton_1_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "9");
+			}
+		});
+		btnNewButton_1_7.setBounds(112, 165, 53, 42);
+		contentPane.add(btnNewButton_1_7);
+		
+		JButton btnNewButton_1_8 = new JButton("0");
+		btnNewButton_1_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + "0");
+			}
+		});
+		btnNewButton_1_8.setBounds(64, 210, 50, 42);
+		contentPane.add(btnNewButton_1_8);
+		
+		JButton btnNewButton_1_9 = new JButton("X");
+		btnNewButton_1_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + " * " );
+			}
+		});
+		btnNewButton_1_9.setBounds(175, 75, 50, 53);
+		contentPane.add(btnNewButton_1_9);
+		
+		JButton btnNewButton_1_10 = new JButton("/");
+		btnNewButton_1_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + " / ");
+			}
+		});
+		btnNewButton_1_10.setBounds(235, 75, 50, 53);
+		contentPane.add(btnNewButton_1_10);
+		
+		JButton btnNewButton_1_11 = new JButton("+");
+		btnNewButton_1_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + " + ");
+			}
+		});
+		btnNewButton_1_11.setBounds(175, 139, 50, 53);
+		contentPane.add(btnNewButton_1_11);
+		
+		JButton btnNewButton_1_5_1 = new JButton(".");
+		btnNewButton_1_5_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + ".");
+			}
+		});
+		btnNewButton_1_5_1.setBounds(15, 210, 50, 42);
+		contentPane.add(btnNewButton_1_5_1);
+		
+		JButton btnNewButton_1_11_1 = new JButton("-");
+		btnNewButton_1_11_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + " - ");
+			}
+		});
+		btnNewButton_1_11_1.setBounds(235, 139, 50, 53);
+		contentPane.add(btnNewButton_1_11_1);
+		
+		JButton btnNewButton_1_11_2 = new JButton("=");
+		btnNewButton_1_11_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				testCase = Display.getText();
+				Display.setText(Display.getText() + " =");
+				
+				
+				CalculatorV5 runEquation = new CalculatorV5();
+				double num = runEquation.run(testCase);
+				Display.removeAll();
+				Display.setText(Display.getText() + num);
+				Display.remove(btnNewButton_1_11_2);
+				
+			}
+		});
+		btnNewButton_1_11_2.setBounds(175, 197, 110, 53);
+		contentPane.add(btnNewButton_1_11_2);
+		
+		Display = new JTextField();
+		Display.setFont(new Font("Arial", Font.BOLD, 39));
+		Display.setBounds(10, 11, 275, 53);
+		contentPane.add(Display);
+		Display.setColumns(10);
+		
+		JButton btnNewButton_1_8_1 = new JButton("C");
+		btnNewButton_1_8_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Display.setText(null);
+
+				
+			}
+		});
+		btnNewButton_1_8_1.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 18));
+		btnNewButton_1_8_1.setBounds(115, 210, 50, 42);
+		contentPane.add(btnNewButton_1_8_1);
+		
+		JButton btnNewButton_1_8_2 = new JButton("(");
+		btnNewButton_1_8_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Display.setText(Display.getText() + " ( ");
+			}
+		});
+		btnNewButton_1_8_2.setBounds(15, 253, 50, 42);
+		contentPane.add(btnNewButton_1_8_2);
+		
+		JButton btnNewButton_1_8_3 = new JButton(")");
+		btnNewButton_1_8_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + " ) ");
+			}
+		});
+		btnNewButton_1_8_3.setBounds(64, 253, 50, 42);
+		contentPane.add(btnNewButton_1_8_3);
+		
+		JButton btnNewButton_1_8_4 = new JButton("^");
+		btnNewButton_1_8_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Display.setText(Display.getText() + " ^ ");
+			}
+		});
+		btnNewButton_1_8_4.setBounds(115, 253, 50, 42);
+		contentPane.add(btnNewButton_1_8_4);
+		
+	}
+}
---
+++ CalclatorGUI/CalculatorV5.java
@@ -0,0 +1,227 @@
+package engine;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class CalculatorV5 {
+	private ArrayList<String> formulaSplitAll = new ArrayList<String>();
+	private String[] ArrayForm;
+	private ArrayList<String> formulaSplitInBrackets = new ArrayList<String>();
+//	private ArrayList<String> formulaSplitOutBrackets = new ArrayList<String>();
+	public int counter = 0;
+	
+//	public CalculatorV4() {
+//		super();
+//		this.testCase = super.testCase;
+//		
+//	}
+	
+	public  ArrayList<String> converStrToListNoBrack(String testCase){
+		// create an array of the equation that is in a string format and split by the spaces
+		ArrayForm = testCase.split(" ");
+		//add all elements from the array to an array list
+		for (int i = 0; i < ArrayForm.length; i++ ) {
+			formulaSplitAll.add(ArrayForm[i]);
+			if (formulaSplitAll.get(i).equals("(")) {
+				counter = counter + 1;
+			}
+		}
+		return formulaSplitAll;
+	}
+	
+	public ArrayList<String> converStrToListWithBrack(ArrayList<String> formulaSplitAll) {
+		
+		
+		while (counter != 0) {
+			int indexOfOpen = formulaSplitAll.indexOf("(");;
+			int indexOfClose = formulaSplitAll.indexOf(")");
+			System.out.println(indexOfOpen);
+			System.out.println(indexOfClose);
+			formulaSplitInBrackets.clear();
+			
+			//add elements stored in the brackets from the "formulaSplitAll" array list into "formulaSplitInBrackets" 
+			for (int i= indexOfOpen ; i< indexOfClose + 1; i++ ) {
+				formulaSplitInBrackets.add(formulaSplitAll.get(i));
+			}
+			System.out.println(formulaSplitAll);
+			System.out.println(formulaSplitInBrackets);
+			// do the necessary calculations of the equations 
+			division(formulaSplitInBrackets);
+			multiplication(formulaSplitInBrackets);
+			addition(formulaSplitInBrackets);
+			subtraction(formulaSplitInBrackets);
+			System.out.println(formulaSplitInBrackets);
+		
+
+			// clear out the equation stored in the brackets in the formulaSplitAll array list 
+			formulaSplitAll.subList(indexOfOpen, indexOfClose +1 ).clear();
+
+			// from the formulaSplitInBrackets, the calculated value from the brackets is added to the formulaSplitAll list
+
+			formulaSplitInBrackets.remove("(");
+			formulaSplitInBrackets.remove(")");
+			System.out.println(formulaSplitInBrackets);
+			System.out.println(formulaSplitAll);
+			
+			for (int j = 0 ; j <= formulaSplitInBrackets.size() - 1; j ++) {
+				formulaSplitAll.add(indexOfOpen , formulaSplitInBrackets.get(j));;
+			}
+			
+			System.out.println(formulaSplitAll);
+			System.out.println(indexOfOpen);
+			System.out.println("size" + formulaSplitAll.size());
+			for (int j = 0; j <= formulaSplitAll.size() - 1 ; j++) {
+				if (indexOfOpen > 0 ) {
+					if (!formulaSplitAll.get(indexOfOpen - 1).equals("+") && 
+							!formulaSplitAll.get(indexOfOpen - 1).equals("/") &&
+							!formulaSplitAll.get(indexOfOpen - 1).equals("*") && 
+							!formulaSplitAll.get(indexOfOpen - 1).equals("-") && 
+							!formulaSplitAll.get((indexOfOpen) - 1).equals("^")){
+						double numInBrack = Double.parseDouble(formulaSplitAll.get(indexOfOpen));
+						System.out.println(numInBrack);
+						double numOutBrack = Double.parseDouble(formulaSplitAll.get(indexOfOpen - 1));
+						numOutBrack = numInBrack * numOutBrack;
+						System.out.println(numOutBrack);
+						String changes = Double.toString(numOutBrack);
+						System.out.println(formulaSplitAll);
+						formulaSplitAll.subList(indexOfOpen - 1, indexOfOpen + 1).clear();
+						formulaSplitAll.add(indexOfOpen - 1, changes);
+						System.out.println(formulaSplitAll);
+						indexOfOpen = 0;
+					}
+				}
+			}
+
+			// if there is a number before the opening brackets, it multiplies the number with the calculated value in brackets 
+			// e.g if equation is ; 2(2), output will be 4 ( multiples 2 by 2 )			
+			// remove the opening and closing brackets symbol
+
+			System.out.println(formulaSplitAll);
+			counter = counter - 1;
+
+			
+		}
+		return formulaSplitAll;
+	}
+
+	
+	// from the order BIDMAS, it looks for the division symbol and divides the numbers before and after the "/" symbol
+	public void division(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			while (m.get(i).equals("/")) {
+				int indexOfThatSymbol = m.indexOf("/");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = (num1 / num2);
+				String changed = Double.toString(num3);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				i = 0;
+				}	
+		}
+	}
+	//it looks for the division symbol and multiplies the numbers before and after the "*" symbol
+	public void multiplication(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			while (m.get(i).equals("*")) {
+				int indexOfThatSymbol = m.indexOf("*");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = (num1 * num2);
+				String changed = Double.toString(num3);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				i = 0;
+				}	
+		}
+	}
+	//it looks for the division symbol and adds the numbers before and after the "+" symbol
+	public void addition(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			while(m.get(i).equals("+")) {
+				int indexOfThatSymbol = m.indexOf("+");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = (num1 + num2);
+				String changed = Double.toString(num3);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				i = 0;
+				}	
+		}
+	}
+	//it looks for the division symbol and subtracts the numbers before and after the "-" symbol
+	public  void subtraction(ArrayList<String> m) {
+		for (int i = 0 ; i <= m.size() - 1; i ++) {
+			while(m.get(i).equals("-")) {
+				int indexOfThatSymbol = m.indexOf("-");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = (num1 - num2);
+				String changed = Double.toString(num3);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				i = 0;
+				}	
+		}
+	}
+	public void power(ArrayList<String> m) {
+		for (int i = 0; i <= m.size() - 1 ; i++) {
+			while(m.get(i).contentEquals("^")) {
+				int indexOfThatSymbol = m.indexOf("^");
+				double num1 = Double.parseDouble(m.get(indexOfThatSymbol - 1));
+				double num2 = Double.parseDouble(m.get(indexOfThatSymbol + 1));
+				double num3 = Math.pow(num1, num2);
+				String changed = Double.toString(num3);
+				m.subList((indexOfThatSymbol - 1), (indexOfThatSymbol + 2)).clear();
+				m.add(indexOfThatSymbol - 1, changed);
+				i = 0;
+			}
+		}
+	}
+	
+	public double run(String testCase){
+
+		
+		CalculatorV5 calc = new CalculatorV5();
+		testCase = testCase.trim();
+		calc.converStrToListNoBrack(testCase);
+		
+//		System.out.println(calc.counter);
+		//look for if the formulaSplitAll array list has any brackets operator, and if it does, carry out the function with the brackets 
+		for (int i = 0; i <= calc.formulaSplitAll.size() - 1 ; i ++ ) {
+			while (calc.formulaSplitAll.contains("(")) {
+				calc.converStrToListWithBrack(calc.formulaSplitAll);
+			}
+		}
+		calc.power(calc.formulaSplitAll);
+		calc.division(calc.formulaSplitAll);
+		calc.multiplication(calc.formulaSplitAll);
+		calc.addition(calc.formulaSplitAll);
+		calc.subtraction(calc.formulaSplitAll);
+		double num = 0;
+		int num2 = 0;
+		// gives a cleaner output with either an integer value or double 
+		for (String b : calc.formulaSplitAll) {
+			num = Double.parseDouble(b);
+			if(num % 1 == 0) {
+				num2 = (int) num;
+				return num2;
+//				System.out.println("Final answer: " + num2);
+				
+			}
+			else {
+//				System.out.println("Final answer: " + num);
+				return num;
+				
+			}
+			
+			
+		}
+		return 0;
+		
+		
+	}
+	
+} 
