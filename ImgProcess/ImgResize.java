5b2a2b9f85c5928656fda68700cb96ca04cfff0f
OsamaBamatraf <60716440+OsamaBamatraf@users.noreply.github.com>
2020-03-15 15:43:22 -0600

    Updated ImgResize

--- ImgProcess/ImgResize.java
+++ ImgProcess/ImgResize.java
@@ -1,4 +1,5 @@
 package ImgProcess;
+
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -8,41 +9,36 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.*;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 import javax.imageio.*;
 
 public class ImgResize {
 	
 	byte[] imgData;
-	int width;
-	int height;
+	private int width = 28;
+	private int height = 28;
 	
-	public ImgResize(byte[] img, int widthReq, int heightReq){
+	public ImgResize(byte[] img){
 		
 		imgData = img;
-		this.width = widthReq;
-		this.height = heightReq;
+		
 	}
 
  public byte[] reSize() throws IOException {
 	 
 	 ByteArrayInputStream inImg = new ByteArrayInputStream(imgData);
 	 BufferedImage imgEdit = ImageIO.read(inImg);
-     if(height == 0) {
-         height = (width * imgEdit.getHeight())/ imgEdit.getWidth(); 
-     }
-     if(width == 0) {
-         width = (height * imgEdit.getWidth())/ imgEdit.getHeight();
-     }
+
      Image resizedImage = imgEdit.getScaledInstance(width, height, Image.SCALE_SMOOTH);
      BufferedImage imageFinal = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
      resizedImage.getGraphics().drawImage(resizedImage, 0, 0, new Color(0,0,0), null);
 
-     ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+     ByteArrayOutputStream outImg = new ByteArrayOutputStream();
 
-     ImageIO.write(imageFinal, "jpg", buffer);
+     ImageIO.write(imageFinal, "jpg", outImg);
 
-     return buffer.toByteArray();
+     return outImg.toByteArray();
 	 
 	 
  }
