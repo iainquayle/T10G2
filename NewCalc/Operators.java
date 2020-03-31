6012f2f7253e86aef058b1196f0d9fcc9f4d2ef2
ParthPatel100 <60551469+ParthPatel100@users.noreply.github.com>
2020-03-31 11:51:05 -0600

    Update Operators.java

--- NewCalc/Operators.java
+++ NewCalc/Operators.java
@@ -1,15 +1,7 @@
 import java.util.ArrayList;
 
-public abstract class Operators extends Equation {
+public abstract class Operators{
 	
-	public Operators(String testCase) {
-		super(testCase);
-	}
-
 	public abstract ArrayList<String> operation(ArrayList<String> m );
-	
-	public void calculate() {
-		this.operation(super.getFormulaSplitAll());
-	}
 	
 }
