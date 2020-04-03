b7c6c62d1d28f35a67293d9fe1b34254edc8335a
OsamaBamatraf <60716440+OsamaBamatraf@users.noreply.github.com>
2020-04-03 03:14:05 -0600

    docs

--- NewCalc/Multiply.java
+++ NewCalc/Multiply.java
@@ -3,7 +3,13 @@ import java.util.ArrayList;
 public class Multiply extends Operators{
 
 
-
+	/**
+	 * This method checks if the formula has a multiplication symbol
+	 * It multiplies what's before and after the multiplication symbol and replaces them with the product
+	 * @return the updated arraylist
+	 */
+	
+	
 	public ArrayList<String> operation(ArrayList<String> m) {
 
 		while(m.contains("*")) {
