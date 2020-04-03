1e9c99fc2264c2885f7f962b3d2532f023506528
OsamaBamatraf <60716440+OsamaBamatraf@users.noreply.github.com>
2020-04-03 01:57:03 -0600

    docs

--- NewCalc/Operators.java
+++ NewCalc/Operators.java
@@ -1,7 +1,17 @@
 import java.util.ArrayList;
-
+/**
+ * Abstract class for use by the operator classes
+ * @author Parth Patel
+ * @documenter
+ *
+ */
 public abstract class Operators{
 	
+	/**
+	 * abstract method for use by the operators
+	 * @param m the list containing the formulas
+	 * @return usually would be the result of the operation
+	 */
 	public abstract ArrayList<String> operation(ArrayList<String> m );
 	
 }
