ae9cb0d0acb94b7da8053519a31ef59608da10d1
OsamaBamatraf <60716440+OsamaBamatraf@users.noreply.github.com>
2020-04-03 03:08:17 -0600

    docs

--- TextNetMulti/Functions.java
+++ TextNetMulti/Functions.java
@@ -1,16 +1,39 @@
 package engine;
 
+/**
+ * Various methods and activation functions for the neurons
+ * @author Iain Quayle
+ * @documenter Osama Bamatraf
+ *
+ */
 public class Functions 
 {
+	
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float sigmoid(float n)
 	{
 		n = clip((float)1.0E6, (float)-1.0E6, n);
 		return n/(float)Math.sqrt(1 + n * n);
 	}
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float sigmoidZer(float n)
 	{
 		return (sigmoid(n) + (float)1) / (float)2;
 	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float relu(float n)
 	{
 		if(n >= 0)
@@ -18,6 +41,11 @@ public class Functions
 		else
 			return (float)0;
 	}
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float leakyRelu(float n)
 	{
 		if(n >= 0)
@@ -25,6 +53,11 @@ public class Functions
 		else
 			return (float)0.001 * n;
 	}
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float leakyRelu6(float n)
 	{
 		if(n >= 1)
@@ -34,10 +67,22 @@ public class Functions
 		else
 			return (float)0.001 * n;
 	}
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float relu6(float n)
 	{
 		return clip(6, 0, n);
 	}
+	/**
+	 * 
+	 * @param upper
+	 * @param lower
+	 * @param n
+	 * @return
+	 */
 	final public static float clip(float upper, float lower, float n)
 	{
 		if(n >= upper)
@@ -53,6 +98,11 @@ public class Functions
 			return n;
 		}
 	}
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float stepNeg(float n)
 	{
 		if(n >= 0)
@@ -60,6 +110,11 @@ public class Functions
 		else
 			return (float)-1;
 	}
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
 	final public static float stepZer(float n)
 	{
 		if(n >= 0)
