f6d4701a213e4da2e4ff580cd5f053ab33d84410
OsamaBamatraf <60716440+OsamaBamatraf@users.noreply.github.com>
2020-04-03 03:11:27 -0600

    docs

--- NewCalc/Equation.java
+++ NewCalc/Equation.java
@@ -8,7 +8,10 @@ public class Equation {
 	
 	protected int counter = 0;
 
-	
+	/**
+	 * constructor initializes the instance variables through the setters in order to prevent privacy leaks
+	 * @param testCase
+	 */
 	public Equation(String testCase) {
 		setEquation(testCase);
 		setArrayForm();
@@ -16,26 +19,46 @@ public class Equation {
 		checkForBrack();
 	
 	}
-	
+	/**
+	 * gets equation
+	 * @return
+	 */
 	public String getEquation() {
 		return this.equation;
 	}
-
+	/**
+	 * sets the value of equation
+	 * @param equation - the equation
+	 */
 	public void setEquation(String equation) {
 		this.equation = equation;
 	}
+	
+	/**
+	 * gets ArrayForm
+	 * @return ArrayForm
+	 */
 	public String[] getArrayForm() {
 		return ArrayForm;
 	}
-	
+	/**
+	 * Sets ArrayForm
+	 * splits the original equation string and places it into an array
+	 */
 	public void setArrayForm() {
 		ArrayForm = getEquation().split(" ");
 	}
-
+	/**
+	 * gets formulaSplitAll
+	 * @return
+	 */
 	public ArrayList<String> getFormulaSplitAll() {
 		return formulaSplitAll;
 	}
-
+	/**
+	 * Sets formulaSplitAll
+	 * Splits every element in the formula into a seperate index
+	 */
 	public void setFormulaSplitAll() {
 		for(int i = 0; i < getArrayForm().length ; i++) {
 			formulaSplitAll.add(getArrayForm()[i]);
@@ -44,7 +67,11 @@ public class Equation {
 	}
 		
 	
-	
+	/**
+	 * Checks if the equations has a bracket
+	 * Counts how many brackets there are
+	 * @return the counts of how many brackets there are
+	 */
 	public int checkForBrack() {
 		for (int i = 0; i < getArrayForm().length; i++) {
 			if(getArrayForm()[i].equals("(")) {
