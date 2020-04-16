39e198fa809b25226f618bc533b5303489527396
ParthPatel100 <60551469+ParthPatel100@users.noreply.github.com>
2020-04-16 12:42:43 -0600

    changed one line, avoid closing all

--- NewCalc/GUI.java
+++ NewCalc/GUI.java
@@ -52,7 +52,7 @@ public class GUI extends JFrame {
 	 * @return 
 	 */
 	public GUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 330, 407);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
